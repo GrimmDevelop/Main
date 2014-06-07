bdcfc5558e078796f012db6f6216612ccad22c98
Freeeed <friedrich@friedrichs-mbp.fritz.box>
2014-06-07 17:28:05 +0200

    Added catalyst migration

--- app/views/login/loginform.blade.php
+++ app/views/login/loginform.blade.php
@@ -10,7 +10,7 @@
     <title>ParkCMS Login</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{{ URL::to('assets/css/main.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/css/main.css') }}" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <style>
@@ -18,7 +18,7 @@
     </style>
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>⁄
+    <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
@@ -30,10 +30,10 @@
     <form action="{{ url('login/auth') }}" method="post" class="col-md-4 col-md-offset-4" role="form">
         <h2 class="form-signin-heading">Anmeldung</h2>
         @if (Session::has('auth_error'))
-            <div class="alert alert-danger">{{ trans('auth.'.Session::get('auth_error')) }}</div>
+        <div class="alert alert-danger">{{ trans('auth.'.Session::get('auth_error')) }}</div>
         @endif
         @if (Session::has('auth_msg'))
-            <div class="alert alert-success">{{ trans('auth.'.Session::get('auth_msg')) }}</div>
+        <div class="alert alert-success">{{ trans('auth.'.Session::get('auth_msg')) }}</div>
         @endif
         <input type="text" name="username" class="form-control" placeholder="Benutzername" required autofocus>
         <input type="password" name="password" class="form-control" placeholder="Passwort" required>
