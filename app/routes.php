70371d0c36a75915efd8e3973cf277c00480889f
Freeeed <friedrich@friedrichs-mbp.fritz.box>
2014-06-21 10:29:48 +0200

    Git route bug

---
+++ app/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+require_once('routes_admin.php');
+require_once('routes_api.php');
+
+Route::get('/', function() {
+    return Redirect::to(URL::to('search'));
+});
+
+Route::get('/search', 'Grimm\Controller\SearchController@searchForm');
+
+Route::post('/search', 'Grimm\Controller\SearchController@searchResult');
