a213a16d83ecf7f94a2f902c8594971df7944cb9
Freeeed <friedrich@ueberreiter.com>
2014-10-19 16:16:54 +0200

    improved location search result overview

--- app/views/admin/partials/searchLocation.blade.php
+++ app/views/admin/partials/searchLocation.blade.php
@@ -9,10 +9,27 @@
         <span>
             <a href ng-click="search(locationName)"><span class="glyphicon glyphicon-search"></span></a>
         </span>
-        <div ng-repeat="location in resultList">
-            <a href ng-click="select(location)">@{{ location.name }} @{{ location.latitude }} @{{ location.longitude }}</a>
-            <a href ng-click="show(location)"><span class="glyphicon glyphicon-map-marker"></span></a>
-        </div>
+
+        <table class="table" ng-show="resultList.length > 0">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>coords.</th>
+                    <th>Map</th>
+                    <th>popul.</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr ng-repeat="location in resultList | orderObjectBy:'population':1">
+                    <td>@{{ location.id }}</td>
+                    <td><a href ng-click="select(location)">@{{ location.name }}</a></td>
+                    <td>@{{ location.latitude }} @{{ location.longitude }}</td>
+                    <td><a href ng-click="show(location)"><span class="glyphicon glyphicon-map-marker"></span></a></td>
+                    <td>@{{ location.population | population }}</td>
+                </tr>
+            </tbody>
+        </table>
     </li>
     </ul>
 </div>
