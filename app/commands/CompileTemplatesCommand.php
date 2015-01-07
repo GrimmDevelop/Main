d9e4167b5a0f08ad9c160aaffe15205fa9a500be
Freeeed <friedrich@Friedrichs-MBP.fritz.box>
2015-01-07 15:27:18 +0100

    Improved compile command
    compile command compiles now correctly all templates in subdirectories

--- app/commands/CompileTemplatesCommand.php
+++ app/commands/CompileTemplatesCommand.php
@@ -37,17 +37,46 @@ class CompileTemplatesCommand extends Command {
      */
     public function fire()
     {
-        if (File::exists(base_path() . '/app/views/admin/compiled.blade.php'))
-        {
+        $this->compileFrontendTemplates();
+        $this->compileAdminTemplates();
+    }
+
+    protected function compileFrontendTemplates()
+    {
+        if (File::exists(base_path() . '/app/views/compiled.blade.php')) {
+            File::delete(base_path() . '/app/views/compiled.blade.php');
+        }
+        $finder = new Finder();
+        $views = $finder->files()->in(base_path() . '/app/views/partials')->name('*.blade.php');
+
+        foreach ($views as $view) {
+            if ($view->getRelativePath() != '') {
+                $path = 'partials/' . str_replace(['/', '\\'], '.', $view->getRelativePath()) . '.' . basename($view->getFilename(), '.blade.php');
+            } else {
+                $path = 'partials/' . basename($view->getFilename(), '.blade.php');
+            }
+
+            // var_dump($path);
+            File::append(base_path() . '/app/views/compiled.blade.php', '<script type="text/ng-template" id="' . $path . '">' . $view->getContents() . '</script>');
+        }
+    }
+
+    protected function compileAdminTemplates()
+    {
+        if (File::exists(base_path() . '/app/views/admin/compiled.blade.php')) {
             File::delete(base_path() . '/app/views/admin/compiled.blade.php');
         }
         $finder = new Finder();
         $views = $finder->files()->in(base_path() . '/app/views/admin/partials')->name('*.blade.php');
 
-        foreach ($views as $view)
-        {
-            $path = 'admin/partials/' . basename($view->getFilename(), '.blade.php');
-            //var_dump($path);
+        foreach ($views as $view) {
+            if ($view->getRelativePath() != '') {
+                $path = 'admin/partials/' . str_replace(['/', '\\'], '.', $view->getRelativePath()) . '.' . basename($view->getFilename(), '.blade.php');
+            } else {
+                $path = 'admin/partials/' . basename($view->getFilename(), '.blade.php');
+            }
+
+            // var_dump($path);
             File::append(base_path() . '/app/views/admin/compiled.blade.php', '<script type="text/ng-template" id="' . $path . '">' . $view->getContents() . '</script>');
         }
     }
