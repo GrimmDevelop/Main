68e479657b8986d3d6d309bcf96f2c4b0da4f4de
Freeeed <friedrich@ueberreiter.com>
2015-03-11 00:39:44 +0100

    Hide loading indicator

--- assets/src/frontend/js/services/Cluster.js
+++ assets/src/frontend/js/services/Cluster.js
@@ -2,7 +2,9 @@ grimmApp.service("Cluster", ['$http', 'BASE_URL', function ($http, BASE_URL) {
     var serviceBackend = BASE_URL + '/api/cluster/';
 
     this.haveChanges = function () {
-        return $http.get(serviceBackend + 'changes');
+        return $http.get(serviceBackend + 'changes', {
+            hideIndicator: true
+        });
     };
 
     this.publish = function() {
